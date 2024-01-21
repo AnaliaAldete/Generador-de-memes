e39080807d7b362f397e29fb0e35e9cb03cc05d1
Analia <ana_292_1@hotmail.com>
2024-01-21 18:46:44 -0300

    agregue funcionalidades del panel de texto

---
+++ README.md
@@ -0,0 +1,41 @@
+# Generador de Memes
+
+Este proyecto es un Generador de Memes basado en HTML, CSS y JavaScript. Puedes crear memes personalizados con imágenes y texto, ajustando diversos parámetros como filtros, estilos de texto, colores, entre otros.
+
+## Estructura del Proyecto
+
+- **index.html:** Contiene la estructura HTML del generador de memes, con secciones para la cabecera, la edición del meme y paneles para la imagen y el texto.
+- **style.css:** Archivo CSS que proporciona estilos visuales para el generador de memes en los modos oscuro y claro.
+- **main.js:** Archivo JavaScript que maneja la lógica del generador de memes, incluyendo la carga de imágenes, ajuste de filtros, manipulación de texto y descarga de memes.
+
+## Modos Oscuro y Claro
+
+El generador de memes tiene dos modos: oscuro y claro. Puedes alternar entre ellos haciendo clic en el botón "Modo oscuro / Modo claro" en la parte superior del generador.
+
+## Paneles de Imagen y Texto
+
+### Panel de Imagen
+
+Permite cargar una imagen desde una URL o desde tu computadora. También proporciona opciones para ajustar el fondo, aplicar filtros y descargar el meme.
+
+### Panel de Texto
+
+Permite agregar y personalizar el texto superior e inferior del meme, seleccionar la fuente, ajustar el tamaño y color del texto, y configurar otros estilos de texto.
+
+## Cómo Usar
+
+1. Selecciona la pestaña "Imagen" o "Texto" según lo que desees editar.
+2. Personaliza la imagen o el texto según tus preferencias.
+3. Ajusta los filtros, estilos y colores según desees.
+4. Descarga el meme creado haciendo clic en el botón "Descargar meme".
+
+## Personalización Avanzada
+
+El proyecto ofrece una variedad de opciones avanzadas para personalizar los memes, desde ajustes de filtro hasta cambios en la fuente y estilo del texto.
+
+## Notas Importantes
+
+- Asegúrate de tener una conexión a Internet para cargar imágenes desde URL.
+- Algunos ajustes pueden afectar la apariencia del meme, experimenta con ellos para obtener el resultado deseado.
+
+¡Diviértete creando memes con este generador personalizable!
--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const panelImg = document.getElementById("panel-img")
 const panelTexto = document.getElementById("panel-texto")
 const btnCambiarModo = document.getElementById("btn-cambiar-modo")
 const body = document.body
-const containerImgMeme = document.getElementById("container-img-meme")
-// const imgMeme=document.getElementById("img-meme")
+const containerMeme = document.getElementById("container-meme")
+const imgMeme = document.getElementById("img-meme")
 const inputUrl = document.getElementById("input-url")
 const inputFondo = document.getElementById("input-fondo")
 const selectTipoFondo = document.getElementById("select-tipo-fondo")
@@ -18,19 +18,39 @@ const inputSepia = document.getElementById("input-sepia")
 const inputHue = document.getElementById("input-hue")
 const inputSaturado = document.getElementById("input-saturado")
 const inputNegativo = document.getElementById("input-negativo")
-const btnFiltros= document.getElementById("btn-filtros")
+const btnFiltros = document.getElementById("btn-filtros")
 const inputCargarImg = document.getElementById("input-cargar-img")
 const btnDescargarMeme = document.getElementById("boton-descargar-meme")
-const inputTextoArriba=document.getElementById("input-texto-arriba")
-const inputTextoAbajo=document.getElementById("input-texto-abajo")
-const textoSuperior= document.getElementById("texto-superior")
-const textoInferior= document.getElementById("texto-inferior")
+const inputTextoArriba = document.getElementById("input-texto-arriba")
+const inputTextoAbajo = document.getElementById("input-texto-abajo")
+const textoSuperior = document.getElementById("texto-superior")
+const textoInferior = document.getElementById("texto-inferior")
+const spanColorFondoImg = document.getElementById("span-color-fondo-img")
+const inputQuitarTextoSuperior = document.getElementById("quitar-texto-superior")
+const inputQuitarTextoInferior = document.getElementById("quitar-texto-inferior")
+const selectFuente = document.getElementById("seleccionar-fuente")
+const inputTamanioLetra = document.getElementById("input-tamanio-letra")
+const btnAlineacionIzquierda = document.getElementById("alineacion-izquierda")
+const btnAlineacionCentro = document.getElementById("alineacion-centro")
+const btnAlineacionDerecha = document.getElementById("alineacion-derecha")
+const inputColorLetra = document.getElementById("input-color-letra")
+const spanColorLetra = document.getElementById("span-color-letra")
+const inputColorFondo = document.getElementById("input-color-fondo")
+const spanColorFondoTexto = document.getElementById("span-color-fondo-texto")
+const inputFondoTransparente = document.getElementById("input-fondo-transparente")
+const inputEspaciado = document.getElementById("input-espaciado")
+const selectInterlineado = document.getElementById("seleccionar-interlineado")
+const btnSinContorno = document.getElementById("btn-sin-contorno")
+const btnContornoClaro = document.getElementById("btn-contorno-claro")
+const btnContornoOscuro = document.getElementById("btn-contorno-oscuro")
+
 
 
 // CAMBIAR PANEL
 btnImg.addEventListener('click', () => {
   panelImg.style.display = 'flex'
   panelTexto.style.display = 'none'
+
 });
 
 btnTexto.addEventListener('click', () => {
@@ -51,11 +71,11 @@ const cambiarTextoModo = () => {
   if (modoActual === "oscuro") {
     btnCambiarModo.innerHTML = '<i class="fa-solid fa-lightbulb"></i> Modo claro'
     btnCambiarModo.setAttribute("data-modo", "claro")
-    
+
   } else {
     btnCambiarModo.innerHTML = '<i class="fa-solid fa-lightbulb"></i> Modo oscuro'
     btnCambiarModo.setAttribute("data-modo", "oscuro")
-    
+
   }
 
 }
@@ -70,20 +90,20 @@ btnCambiarModo.addEventListener('click', cambiarTextoModo)
 
 window.visualViewport.addEventListener("resize", () => {
   if (window.visualViewport.width < 600) {
-    containerImgMeme.style.height = `${window.visualViewport.width - 30}px`
-    containerImgMeme.style.width = `${window.visualViewport.width - 30}px`
+    containerMeme.style.height = `${window.visualViewport.width - 30}px`
+    containerMeme.style.width = `${window.visualViewport.width - 30}px`
   } else {
-    containerImgMeme.style.height = "400px"
-    containerImgMeme.style.width = "400px"
+    containerMeme.style.height = "350px"
+    containerMeme.style.width = "350px"
   }
 })
 
 if (window.visualViewport.width < 600) {
-  containerImgMeme.style.height = `${window.visualViewport.width - 30}px`
-  containerImgMeme.style.width = `${window.visualViewport.width - 30}px`
+  containerMeme.style.height = `${window.visualViewport.width - 30}px`
+  containerMeme.style.width = `${window.visualViewport.width - 30}px`
 } else {
-  containerImgMeme.style.height = "400px"
-  containerImgMeme.style.width = "400px"
+  containerMeme.style.height = "350px"
+  containerMeme.style.width = "350px"
 }
 
 // INICIO PANEL IMAGEN
@@ -92,15 +112,11 @@ if (window.visualViewport.width < 600) {
 inputUrl.addEventListener('input', () => {
 
   let urlCargada = inputUrl.value
-
-  // imgMeme.src = urlCargada
-  containerImgMeme.style.background = `url(${urlCargada})`
-  containerImgMeme.style.backgroundSize="contain"
-
+  imgMeme.style.background = `url(${urlCargada})`
+  imgMeme.style.backgroundSize = "contain"
 
 }
 
-  // containerImgMeme.style.backgroundImage=`url(${urlCargada})`}
 )
 
 //CARGA IMG DE LA COMPU
@@ -110,7 +126,7 @@ inputCargarImg.addEventListener("change", (e) => {
   if (archivo) {
     const reader = new FileReader();
     reader.onload = (e) => {
-      containerImgMeme.style.backgroundImage = `url(${e.target.result})`;
+      imgMeme.style.backgroundImage = `url(${e.target.result})`;
     };
     reader.readAsDataURL(archivo);
   }
@@ -119,28 +135,31 @@ inputCargarImg.addEventListener("change", (e) => {
 //DESCARGAR MEME
 
 btnDescargarMeme.addEventListener("click", () => {
-  domtoimage.toBlob(containerImgMeme).then((blob) => {
+  domtoimage.toBlob(containerMeme).then((blob) => {
     window.saveAs(blob, "meme.png");
   });
 });
 
-//CAMBIA COLOR FONDO DEL MEME
+//CAMBIA COLOR FONDO DEL MEME y actualiza el span con el color
 inputFondo.addEventListener('input', () => {
   let colorFondo = inputFondo.value
-  containerImgMeme.style.backgroundColor = colorFondo
+  imgMeme.style.backgroundColor = colorFondo
+  spanColorFondoImg.innerText = colorFondo
 })
+
+
 
 //CAMBIA TIPO DE FONDO DEL MEME
 selectTipoFondo.addEventListener('change', () => {
   let tipoFondo = selectTipoFondo.value
-  containerImgMeme.style.backgroundBlendMode = tipoFondo
+  imgMeme.style.backgroundBlendMode = tipoFondo
 })
 
 // CAMBIA FILTROS
 
-const aplicarFiltros=()=> {
+const aplicarFiltros = () => {
 
-  containerImgMeme.style.filter = `brightness(${inputBrillo.value}) opacity(${inputOpacidad.value}) contrast(${inputContraste.value}%) blur(${inputDesenfoque.value}px) grayscale(${inputEscalaGrises.value}%) sepia(${inputSepia.value}%) hue-rotate(${inputHue.value}deg) saturate(${inputSaturado.value}%) invert(${inputNegativo.value})`
+  imgMeme.style.filter = `brightness(${inputBrillo.value}) opacity(${inputOpacidad.value}) contrast(${inputContraste.value}%) blur(${inputDesenfoque.value}px) grayscale(${inputEscalaGrises.value}%) sepia(${inputSepia.value}%) hue-rotate(${inputHue.value}deg) saturate(${inputSaturado.value}%) invert(${inputNegativo.value})`
 
 }
 
@@ -155,20 +174,20 @@ inputSaturado.addEventListener("input", aplicarFiltros);
 inputNegativo.addEventListener("input", aplicarFiltros);
 
 //REESTABLECER FILTROS
-const reestablecerFiltros=()=>{
-  inputBrillo.value=1
-  inputOpacidad.value=1
-  inputContraste.value=100
-  inputDesenfoque.value=0
-  inputEscalaGrises.value=0
-  inputSepia.value=0
-  inputHue.value=0
-  inputSaturado.value=100
-  inputNegativo.value=0
-
+const reestablecerFiltros = () => {
+  inputBrillo.value = 1
+  inputOpacidad.value = 1
+  inputContraste.value = 100
+  inputDesenfoque.value = 0
+  inputEscalaGrises.value = 0
+  inputSepia.value = 0
+  inputHue.value = 0
+  inputSaturado.value = 100
+  inputNegativo.value = 0
+aplicarFiltros()
 }
 
-btnFiltros.addEventListener("click",reestablecerFiltros)
+btnFiltros.addEventListener("click", reestablecerFiltros)
 
 //FIN PANEL IMAGEN
 
@@ -176,12 +195,137 @@ btnFiltros.addEventListener("click",reestablecerFiltros)
 
 //ACTUALIZAR TEXTOS DE PARRAFOS
 
-const actualizarTexto=()=>{
-  textoSuperior.innerText= inputTextoArriba.value
-  textoInferior.innerText= inputTextoAbajo.value
-  
+const actualizarTexto = () => {
+  textoSuperior.innerText = inputTextoArriba.value
+  textoInferior.innerText = inputTextoAbajo.value
+
 }
 
 inputTextoArriba.addEventListener("input", actualizarTexto)
 inputTextoAbajo.addEventListener("input", actualizarTexto)
 
+//QUITAR PARRAFOS
+
+const quitarParrafos = () => {
+  if (inputQuitarTextoSuperior.checked) {
+    textoSuperior.style.display = 'none'
+  } else {
+    textoSuperior.style.display = 'block'
+  }
+  if (inputQuitarTextoInferior.checked) {
+    textoInferior.style.display = 'none'
+  } else {
+    textoInferior.style.display = 'block'
+  }
+}
+
+inputQuitarTextoSuperior.addEventListener("change", quitarParrafos)
+inputQuitarTextoInferior.addEventListener("change", quitarParrafos)
+
+//CAMBIAR FUENTE DE PARRAFOS
+
+selectFuente.addEventListener('change', () => {
+
+  let fuente = selectFuente.value
+
+  textoSuperior.style.fontFamily = fuente
+  textoInferior.style.fontFamily = fuente
+})
+
+//CAMBIAR TAMAÑO LETRA DE PARRAFOS
+
+inputTamanioLetra.addEventListener("change", () => {
+  textoSuperior.style.fontSize = `${inputTamanioLetra.value}px`
+  // textoSuperior.style.display = 'flex';
+  // textoSuperior.style.justifyContent = 'center';
+  // textoSuperior.style.alignItems = 'center'
+  textoInferior.style.fontSize = `${inputTamanioLetra.value}px`
+  //   textoInferior.style.display = 'flex';
+  //   textoInferior.style.justifyContent = 'center';
+  //   textoInferior.style.alignItems = 'center'
+}
+)
+
+//CAMBIAR ALINEACION DEL PARRAFOS
+
+btnAlineacionIzquierda.addEventListener("click", () => {
+  textoSuperior.style.textAlign = "left"
+  textoInferior.style.textAlign = "left"
+
+})
+
+btnAlineacionCentro.addEventListener("click", () => {
+  textoSuperior.style.textAlign = "center"
+  textoInferior.style.textAlign = "center"
+})
+
+btnAlineacionDerecha.addEventListener("click", () => {
+  textoSuperior.style.textAlign = "right"
+  textoInferior.style.textAlign = "right"
+})
+
+//CAMBIAR COLOR DE LETRA DE PARRAFOS y actualiza el span con el color
+
+inputColorLetra.addEventListener("input", () => {
+  textoSuperior.style.color = inputColorLetra.value
+  textoInferior.style.color = inputColorLetra.value
+  spanColorLetra.innerText = inputColorLetra.value
+})
+
+//CAMBIAR COLOR DE FONDO DE PARRAFOS y actualiza el span con el color
+
+inputColorFondo.addEventListener("input", () => {
+  textoSuperior.style.backgroundColor = inputColorFondo.value
+  textoInferior.style.backgroundColor = inputColorFondo.value
+  spanColorFondoTexto.innerText = inputColorFondo.value
+})
+
+//FONDO TRANSPARENTE
+
+const fondoTransparente = () => {
+  if (inputFondoTransparente.checked) {
+    textoSuperior.style.backgroundColor = inputFondo.value
+    textoInferior.style.backgroundColor = inputFondo.value
+  } else {
+    textoSuperior.style.backgroundColor = inputColorFondo.value
+    textoInferior.style.backgroundColor = inputColorFondo.value
+  }
+}
+
+inputFondoTransparente.addEventListener("change", fondoTransparente)
+
+//CAMBIAR CONTORNO DEL TEXTO
+
+btnSinContorno.addEventListener("click", () => {
+  textoSuperior.style.textShadow = "none"
+  textoInferior.style.textShadow = "none"
+})
+
+btnContornoClaro.addEventListener("click", () => {
+  textoSuperior.style.textShadow = "2px 2px 2px white"
+  textoInferior.style.textShadow = "2px 2px 2px white"
+
+})
+
+btnContornoOscuro.addEventListener("click", () => {
+  textoSuperior.style.textShadow = "2px 2px 2px black"
+  textoInferior.style.textShadow = "2px 2px 2px black"
+
+})
+
+
+//CAMBIAR ESPACIADO
+
+inputEspaciado.addEventListener("input", () => {
+  textoSuperior.style.padding = `${inputEspaciado.value}px 10px`
+  textoInferior.style.padding = `${inputEspaciado.value}px 10px`
+})
+
+//CAMBIAR INTERLINEADO
+
+selectInterlineado.addEventListener("change", () => {
+  textoSuperior.style.lineHeight = selectInterlineado.value
+  textoInferior.style.lineHeight = selectInterlineado.value
+})
+
+//FIN PANEL DE TEXTO
